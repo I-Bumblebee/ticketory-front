edcbcb22a37ae92f0d0deca3bf416e63151100fb
Ubuntu <ubuntu@ip-172-26-8-157.eu-central-1.compute.internal>
2024-12-01 07:57:02 +0000

    hotfix: missing csrf cookie from headers

--- src/plugins/axios/index.ts
+++ src/plugins/axios/index.ts
@@ -3,6 +3,8 @@ import axios, { AxiosInstance } from 'axios';
 const httpClient: AxiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
   headers: {
+    'X-Requested-With': 'XMLHttpRequest',
+    Accept: 'application/json',
     'Content-Type': 'application/json',
   },
   withCredentials: true,
